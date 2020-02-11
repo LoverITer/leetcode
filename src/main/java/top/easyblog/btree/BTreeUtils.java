75cd793bc84119b69f35cab66387031b4209db7e
LoverITer <2489868503@qq.com>
2020-02-11 22:56:32 +0800

    removeDuplicates

--- src/main/java/top/easyblog/btree/BTreeUtils.java
+++ src/main/java/top/easyblog/btree/BTreeUtils.java
@@ -1,8 +1,31 @@
 package top.easyblog.btree;
 
+import top.easyblog.array.ArrayUtils;
+
+import java.util.Objects;
+
 /**
  * @author HuangXin
  * @since 2020/2/11 21:07
  */
 public class BTreeUtils {
+
+    private static BTreeUtils bTreeUtils = null;
+
+    private BTreeUtils() {
+
+    }
+
+    public static BTreeUtils getInstance() {
+        if (Objects.isNull(bTreeUtils)) {
+            synchronized (ArrayUtils.class) {
+                if (Objects.isNull(bTreeUtils)) {
+                    bTreeUtils = new BTreeUtils();
+                }
+            }
+        }
+        return bTreeUtils;
+    }
+
+
 }
--- src/main/java/top/easyblog/linklist/LinkListUtils.java
+++ src/main/java/top/easyblog/linklist/LinkListUtils.java
@@ -1,8 +1,28 @@
 package top.easyblog.linklist;
 
+import java.util.Objects;
+
 /**
  * @author HuangXin
  * @since 2020/2/11 21:08
  */
 public class LinkListUtils {
+
+    private static LinkListUtils linkListUtils = null;
+
+    private LinkListUtils() {
+
+    }
+
+    public static LinkListUtils getInstance() {
+        if (Objects.isNull(linkListUtils)) {
+            synchronized (LinkListUtils.class) {
+                if (Objects.isNull(linkListUtils)) {
+                    linkListUtils = new LinkListUtils();
+                }
+            }
+        }
+        return linkListUtils;
+    }
+
 }
