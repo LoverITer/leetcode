9d85f504e81804b65ec9b5044fcf6565a4cd31d2
LoverITer <2489868503@qq.com>
2020-02-15 00:46:36 +0800

    removeDuplicates

--- src/main/java/top/easyblog/array/ArrayUtils.java
+++ src/main/java/top/easyblog/array/ArrayUtils.java
@@ -1,7 +1,6 @@
 package top.easyblog.array;
 
 
-
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -36,10 +35,12 @@ public class ArrayUtils {
      * 复制到 nums[i + 1]。然后递增 i,接着我们将再次重复相同的过程,直到 j 到达数组的末尾为止。
      *
      * @param nums 目标数组
-     * @return 没有重复元素的数组
+     * @return int[]
      */
     public int[] removeDuplicates(int[] nums) {
-        if (nums.length == 0) return null;
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
         int i = 0;
         for (int j = 1; j < nums.length; j++) {
             if (nums[j] != nums[i]) {
@@ -48,6 +49,34 @@ public class ArrayUtils {
             }
         }
         return Arrays.copyOf(nums, i + 1);
+    }
+
+
+    /**
+     * 移除一个数组中职位val的数<br/>
+     * 思路：基于遍历数组，当nums[i]==val时，把nums[i]和数组末尾的值交换一下，并释放最后一个数（实际上是然数组的长度-1）
+     * 注意：但是有可能我们最后一个数也是需要删除的。对于这个问题无序担心，因为每次交换会对nums[i]进行两次检查
+     * 复杂度： 时间复杂度O(n)   空间复杂度O(1)
+     *
+     * @param nums 目标数组
+     * @param val  数组中要移除的数
+     * @return  int[]
+     */
+    public int[] removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int len = nums.length;
+        int i = 0;
+        while (i < len) {
+            if (nums[i] == val) {
+                nums[i] = nums[len - 1];
+                len--;
+            } else {
+                i++;
+            }
+        }
+        return Arrays.copyOf(nums, len);
     }
 
 }
---
+++ src/main/java/top/easyblog/string/StringUtils.java
@@ -0,0 +1,12 @@
+package top.easyblog.string;
+
+/**
+ * @author ：huangxin
+ * @modified By：
+ * @Description：TODO
+ * @since ：2020/02/15 00:30
+ */
+public class StringUtils {
+
+
+}
