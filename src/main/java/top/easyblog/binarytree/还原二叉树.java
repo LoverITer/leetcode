a6dd53d6ca690dbe27027c723416e5e433859340
LoverITer <2489868503@qq.com>
2020-07-02 12:02:50 +0800

    binary-tree：rebuild tree from PostOrder&InOrder

--- src/main/java/top/easyblog/binarytree/还原二叉树.java
+++ src/main/java/top/easyblog/binarytree/还原二叉树.java
@@ -14,9 +14,11 @@ import java.util.HashMap;
  */
 public class 还原二叉树 {
 
-
+    /*存储中序遍历的结果和索引之间的映射关系*/
     private static HashMap<Object, Integer> inOrderMap;
 
+    /*先序遍历或后序遍历结果集*/
+    private static String[] order;
     /**
      * 根据前序遍历+中序遍历恢复一颗二叉树
      *
@@ -29,8 +31,8 @@ public class 还原二叉树 {
     }
 
     /**
-     * 根据前序遍历+中序遍历或者后序+中序遍历恢复一颗二叉树
-     *
+     * 根据前序遍历+中序遍历或者后序+中序遍历恢复一颗二叉树<br/>
+     * Tips：此算法由于需要大量的拷贝数组，因此效率和空间利用都不是很友好
      * @param order      前序遍历或后序遍历
      * @param isPreOrder order是否是前序遍历
      * @param inOrder    中序遍历
@@ -97,26 +99,36 @@ public class 还原二叉树 {
     }
 
     /**
-     * 对上面的代码进行空间复杂度优化：实际上并不需要真正的切分原数组，只需要限定数组的范围就好了
-     *
-     * @param order
-     * @param inOrder
+     * 对上面的代码进行空间复杂度优化：实际上并不需要真正的切分原数组，只需要限定数组的范围就好了<br/>
+     * Tips: 解决了需要大量数组拷贝的问题，只需要使用四个int值来限定范围就好了
+     * @param orders 前序或后序序列
+     * @param inOrder 中序序列
      * @param isPreOrder 是否是通过前序遍历还原
      * @return
      */
-    public static TreeNode reConstructBinTree(String[] order, String[] inOrder, boolean isPreOrder) {
-        if (order == null || inOrder == null || inOrder.length == 0 || order.length != inOrder.length) {
+    public static TreeNode reConstructBinTree(String[] orders, String[] inOrder, boolean isPreOrder) {
+        if (orders == null || inOrder == null || inOrder.length == 0 || orders.length != inOrder.length) {
             return null;
         }
+        order = orders;
         inOrderMap = new HashMap<>();
         for (int i = 0; i < inOrder.length; i++) {
             inOrderMap.put(inOrder[i], i);
         }
-        return reBuild(order, 0, order.length - 1, inOrder, 0, order.length - 1, isPreOrder);
+        return reBuild(0, order.length - 1, 0, order.length - 1, isPreOrder);
     }
 
-
-    private static TreeNode reBuild(String[] order, int orderLeft, int orderRight, String[] inOrder, int inOrderLeft, int inOrderRight, boolean isPreOrder) {
+    /**
+     * 已知前序和中序或已知后序和中序遍历还原二叉树
+     *
+     * @param orderLeft    前序或后序序列的开始
+     * @param orderRight   前序或后序序列的结束
+     * @param inOrderLeft  中序遍历的开始
+     * @param inOrderRight 中序遍历的结束
+     * @param isPreOrder   是否是前序+中序的组合
+     * @return
+     */
+    private static TreeNode reBuild(int orderLeft, int orderRight, int inOrderLeft, int inOrderRight, boolean isPreOrder) {
         if (orderLeft > orderRight) {
             return null;
         }
@@ -126,7 +138,7 @@ public class 还原二叉树 {
             rootCharacter = order[orderLeft];
         } else {
             //后序遍历，order最后一个元素是根节点
-            rootCharacter = order[order.length - 1];
+            rootCharacter = order[orderRight];
         }
         //在中序遍历中寻找根节点的索引下标
         int index = inOrderMap.get(rootCharacter);
@@ -134,40 +146,78 @@ public class 还原二叉树 {
         int leftSubTreeSize = index - inOrderLeft;
         //根节点
         TreeNode root = new TreeNode(Integer.parseInt(rootCharacter));
-        /* 本算法的核心就是下面这幅图，也就是在已知的前序和中序中寻找到分别寻找到左子树个右子树的范围即可
-         * ┌--------------------------------------------------------------------------------┐
-         * |    orderLeft           orderRight                                              |
-         * |      ||                 ||                                                     |
-         * |      \/                 \/                                                     |
-         * |先序： A [B C D] [E F G H K]                                                     |
-         * |      /\   /\           /\                                                      |
-         * |      ||   ||           ||                                                      |
-         * |     root "left Child"  "right Child"                                           |
-         * |            || Range          ||Range                                           |
-         * |            ||                \/                                                 |
-         * |            \/            [orderLeft+leftChild.length()+1,orderChild]           |
-         * |         [orderLeft+1,orderLeft + leftChild.length()]                           |
-         * └--------------------------------------------------------------------------------┘
-         * ┌--------------------------------------------------------------------------------┐
-         * |                     [index+1,orderRight]                                       |
-         * |                      /\                                                        |
-         * |                      || Range                                                  |
-         * |    orderLeft        orderRight                                                 |
-         * |      ||              ||                                                        |
-         * |      \/              \/                                                        |
-         * |中序：[B D C] A E H G K F                                                        |
-         * |       /\    /\                                                                 |
-         * |       ||    ||                                                                 |
-         * |"left Child" root,index=4                                                       |
-         * |       || Range                                                                 |
-         * |       \/                                                                       |
-         * |   [orderLeft,index-1]                                                          |
-         * └--------------------------------------------------------------------------------┘
-         */
-        //递归构造左子树
-        root.left = reBuild(order, orderLeft + 1, orderLeft + leftSubTreeSize, inOrder, inOrderLeft, index - 1, isPreOrder);
-        //递归构造右子树
-        root.right = reBuild(order, orderLeft + leftSubTreeSize + 1, orderRight, inOrder, index + 1, inOrderRight, isPreOrder);
+        if (isPreOrder) {
+            /* 先序+中序重构二叉树的核心就是下面这幅图，也就是在已知的前序和中序中寻找到分别寻找到左子树个右子树的范围即可
+             * ┌--------------------------------------------------------------------------------┐
+             * |    orderLeft           orderRight                                              |
+             * |      ||                 ||                                                     |
+             * |      \/                 \/                                                     |
+             * |先序： A [B C D] [E F G H K]                                                     |
+             * |      /\   /\           /\                                                      |
+             * |      ||   ||           ||                                                      |
+             * |     root "left Child"  "right Child"                                           |
+             * |            || Range          ||Range                                           |
+             * |            ||                \/                                                |
+             * |            \/            [orderLeft+leftChild.length()+1,orderRight]           |
+             * |         [orderLeft+1,orderLeft + leftChild.length()]                           |
+             * └--------------------------------------------------------------------------------┘
+             * ┌--------------------------------------------------------------------------------┐
+             * |                     [index+1,orderRight]                                       |
+             * |                      /\                                                        |
+             * |                      || Range                                                  |
+             * |    orderLeft        orderRight                                                 |
+             * |      ||              ||                                                        |
+             * |      \/              \/                                                        |
+             * |中序：[B D C] A E H G K F                                                        |
+             * |       /\    /\                                                                 |
+             * |       ||    ||                                                                 |
+             * |"left Child" root,index=4                                                       |
+             * |       || Range                                                                 |
+             * |       \/                                                                       |
+             * |   [orderLeft,index-1]                                                          |
+             * └--------------------------------------------------------------------------------┘
+             */
+            //递归构造左子树
+            root.left = reBuild(orderLeft + 1, orderLeft + leftSubTreeSize, inOrderLeft, index - 1, true);
+            //递归构造右子树
+            root.right = reBuild(orderLeft + leftSubTreeSize + 1, orderRight, index + 1, inOrderRight, true);
+        } else {
+            /* 后序+中序重构二叉树的核心就是下面这幅图，也就是在已知的前序和中序中寻找到分别寻找到左子树个右子树的范围即可
+             * ┌--------------------------------------------------------------------------------┐
+             * |    orderLeft           orderRight                                              |
+             * |      ||                 ||                                                     |
+             * |      \/                 \/                                                     |
+             * |后序：[D C B] [H K G F E] A      leftChild.length()=index-inOrderLeft            |
+             * |       /\           /\   /\                                                     |
+             * |       ||           ||   ||                                                     |
+             *         ||           ||   root                                                   |
+             * |    "left Child"  "right Child"                                                 |
+             * |        || Range       ||Range                                                  |
+             * |        ||             \/                                                       |
+             * |        \/            [orderLeft+leftChild.length(),orderRight-1]               |
+             * |    [orderLeft,orderLeft + leftChild.length()-1]                                |
+             * └--------------------------------------------------------------------------------┘
+             * ┌--------------------------------------------------------------------------------┐
+             * |                     [index+1,orderRight]                                       |
+             * |                      /\                                                        |
+             * |                      || Range                                                  |
+             * |    orderLeft        orderRight                                                 |
+             * |      ||              ||                                                        |
+             * |      \/              \/                                                        |
+             * |中序：[B D C] A E H G K F                                                        |
+             * |       /\    /\                                                                 |
+             * |       ||    ||                                                                 |
+             * |"left Child" root,index=4                                                       |
+             * |       || Range                                                                 |
+             * |       \/                                                                       |
+             * |   [orderLeft,index-1]                                                          |
+             * └--------------------------------------------------------------------------------┘
+             */
+            //递归构造左子树
+            root.left = reBuild(orderLeft, orderLeft+leftSubTreeSize-1, inOrderLeft, index - 1, false);
+            //递归构造右子树
+            root.right = reBuild(orderLeft + leftSubTreeSize, orderRight - 1, index + 1, inOrderRight, false);
+        }
         return root;
     }
 
@@ -192,10 +242,10 @@ public class 还原二叉树 {
 
         TreeNode root1 = reConstructBinTree(preOrder, inOrder);
         TreeNode root2 = reConstructBinTree(postOrder, false, inOrder);
-        TreeNode root3 = reConstructBinTree(preOrder,inOrder,true);
+        TreeNode root3 = reConstructBinTree(postOrder, inOrder, false);
         System.out.println("root1后序遍历：" + 二叉树后序遍历.postOrder2(root1));
         System.out.println("root2后序遍历：" + 二叉树后序遍历.postOrder2(root2));
-        System.out.println("root4后序遍历：" + 二叉树后序遍历.postOrder2(root3));
+        System.out.println("root3后序遍历：" + 二叉树后序遍历.postOrder2(root3));
     }
 
 }
